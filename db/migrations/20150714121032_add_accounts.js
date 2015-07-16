72c88de2c32eb95a7772f60f8417e5d238dd5bc5
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2015-07-16 04:10:15 -0700

    updated migration file for accounts to start balances at 0

--- db/migrations/20150714121032_add_accounts.js
+++ db/migrations/20150714121032_add_accounts.js
@@ -3,11 +3,11 @@
 exports.up = function(knex, Promise) {
     return Promise.all([
         knex.schema.createTable('accounts', function(table) {
-            table.uuid("id").primary().notNullable();
+            table.uuid("id").primary();
             table.uuid("user_id").notNullable().references("id").inTable("users");
             table.integer("currency_id").notNullable().references("id").inTable("currencies");
-            table.float("balance", 16, 8);
-            table.float("available", 16, 8);
+            table.float("balance", 16, 8).defaultTo(0);
+            table.float("available", 16, 8).defaultTo(0);
         })
     ]);
 };
