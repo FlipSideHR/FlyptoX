c909daee4f35483cada3a54dc19e647343199208
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2015-07-16 01:30:39 -0700

    added order_id to transactions
    to easily lookup the order of the user that resulted in a transaction on their account

--- db/migrations/20150714121035_add_transactions.js
+++ db/migrations/20150714121035_add_transactions.js
@@ -9,6 +9,14 @@ exports.up = function(knex, Promise) {
             table.float("balance", 16, 8);
             table.string("type");
             table.uuid("trade_id").references("id").inTable("trades");
+            //user's order that resulted in this transaction
+            table.uuid("order_id").references("id").inTable("orders");
+
+            //we might need a transfer_id and a new transfers
+            //table which stores a list deposits and withdrawls from and into
+            //accounts (the migration file needs to preceed this one)
+            //table.uuid("trander_id").refrences("id").inTable("transfers");
+
         })
     ]);
 };
