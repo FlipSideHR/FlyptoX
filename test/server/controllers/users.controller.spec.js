e90c5777ab93e3a6d1d6f755f3817cac5b704791
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2015-07-18 00:21:55 -0700

    updated controller tests

--- test/server/controllers/users.controller.spec.js
+++ test/server/controllers/users.controller.spec.js
@@ -2,54 +2,90 @@ var chai = require('chai');
 var expect = chai.expect;
 var should = chai.should();
 
+var bookshelf = require("../../../server/utils/bookshelf");
 var users = require("../../../server/controllers/users.js");
 var helpers = require("../models/helpers");
 var User = require("../../../server/models/User");
 
+function clearTables(done){
+  bookshelf.knex('accounts').del()
+  .then(function(){
+      return bookshelf.knex('orders').del();
+  })
+  .then(function(){
+      return bookshelf.knex('users').del();
+  }).then(function(){
+    done();
+  })
+  .catch(done);
+}
 describe('Users Controller', function(){
-  before(helpers.clean);
+  before(clearTables);
+  after(clearTables);
 
-  describe("Signup process", function(){
-    it("should create a new user", function(done){
-      users.signup("test@email.com","password").then(function(user){
-        expect(user).to.not.equal(undefined);
+  describe("users.signup(email, password)", function(){
+    it("should create a new user and return a token", function(done){
+      users.signup("test@email.com","password").then(function(token){
+        expect(token).to.be.a('string');
         done();
       }).catch(done);
     });
 
-    it("new user should have a two accounts", function(done){
+    it("should create two accounts for user", function(done){
       User.where({email:"test@email.com"})
         .fetch({withRelated:'accounts'})
         .then(function(user){
           expect(user.related('accounts').length).to.equal(2);
-          //delete accounts and user
-          user.related('accounts').at(0).destroy();
-          user.related('accounts').at(1).destroy();
-          user.destroy();
           done();
         }).catch(done);
     });
 
-    xit("new user should have a USD account with balance 0", function(done){
-      done();
+    it("should create USD account with balance 0", function(done){
+      User.where({email:"test@email.com"})
+        .fetch({withRelated:'accounts'})
+        .then(function(user){
+          var usd_account = user.related('accounts').findWhere({currency_id:1});
+          expect(usd_account).to.not.equal(undefined);
+          expect(usd_account.get('balance')).to.equal(0);
+          done();
+        }).catch(done);
     });
 
-    xit("should create a new BTC account with balance 0", function(done){
-      done();
+    it("should create BTC account with balance 0", function(done){
+      User.where({email:"test@email.com"})
+        .fetch({withRelated:'accounts'})
+        .then(function(user){
+          var btc_account = user.related('accounts').findWhere({currency_id:2});
+          expect(btc_account).to.not.equal(undefined);
+          expect(btc_account.get('balance')).to.equal(0);
+          done();
+        }).catch(done);
     });
 
-    xit("should return a new access token", function(done){
-      done();
-    });
   });
 
-  describe("Signin process", function(){
-    xit("should fail for invalid username and password combination", function(done){
-      done();
+  describe("users.signin(email, password)", function(){
+    it("should return access token for valid username and password", function(done){
+      users.signin("test@email.com","password").then(function(token){
+        expect(token).to.be.a('string');
+        done();
+      }).catch(done);
     });
 
-    xit("should return access token for valid username and password combination", function(done){
-      done();
+    it("should throw error for invalid username", function(done){
+      users.signin("wrongusername","password").then(function(token){
+        done(new Error("token returned for invalid account!"));
+      }).catch(function(){
+        done();
+      });
+    });
+
+    it("should throw error for invalid password", function(done){
+      users.signin("test@email.com","wrongpassword").then(function(token){
+        done(new Error("token returned for invalid account!"));
+      }).catch(function(){
+        done();
+      });
     });
   });
 
