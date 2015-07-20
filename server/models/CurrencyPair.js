6b3f64951a75930ac64b3202eeecef91734a3cdf
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2015-07-20 01:20:45 -0700

    fixed error introduced during rebase

--- server/models/CurrencyPair.js
+++ server/models/CurrencyPair.js
@@ -1,7 +1,9 @@
 var bookshelf = require('../utils/bookshelf');
 
+require('./Currency');
+
 module.exports = bookshelf.model('CurrencyPair', {
-  tableName: 'currency_pairs'
+  tableName: 'currency_pairs',
 
   base_currency: function(){
     return this.belongsTo('Currency', 'base_currency_id');
