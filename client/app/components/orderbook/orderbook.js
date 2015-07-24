862d51efddd4c68cd512dbf8ddb1fd70d5b4d9dc
Richard Treichel <rwtreichel@gmail.com>
2015-07-23 22:39:20 -0400

    Show users' orders on the orderbook page. Can cancel them as well.

--- client/app/components/orderbook/orderbook.js
+++ client/app/components/orderbook/orderbook.js
@@ -15,7 +15,28 @@
         size: 0
       };
 
-      $scope.orders = [];
+      $scope.myOrders = [];
+
+      $scope.cancelOrder = function(index) {
+        // Cancel the order at index of $scope.myOrders
+          // Post a delete call to the API for that order by using its id
+          // Refresh the list of 'my orders'...it should not be there anymore
+
+        var orderId = $scope.myOrders[index].id;
+        console.log('orderId', orderId);
+
+        $http({
+          method: 'DELETE',
+          url: '/api/v1/orders/' + orderId
+        })
+        .success(function(data, status, headers, config, statusText){
+          $scope.getOrders();
+          $scope.getBook();
+        })
+        .error(function(data, status, headers, config, statusText){
+
+        });
+      };
 
       $scope.getBook = function () {
         $http({
@@ -37,7 +58,7 @@
           url: '/api/v1/orders'
         })
         .success(function(data, status, headers, config, statusText){
-          $scope.orders = data;     
+          $scope.myOrders = data;
         })
         .error(function(data, status, headers, config, statusText) {
 
@@ -71,6 +92,7 @@
           // statusText – {string} – HTTP status text of the response.
           console.log(data, status, statusText);
           $scope.getBook();
+          $scope.getOrders();
           console.log('Success');
         })
         .error(function(data, status, headers, config, statusText) {
@@ -81,17 +103,22 @@
 
       socket.on('order:new', function(orderData) {
         console.log('Server says there is a new order. Add it!');
-        $scope.orders.push(orderData);
+        $scope.getBook();
       });
 
-      socket.on('order:cancelled', function(orderData) {
-        console.log('Server says an order has been cancelled. Remove it!');
-        for (var i = 0; i < $scope.orders.length; i++) {
-          if ($scope.orders[i].id === orderData.id) {
-            $scope.orders.splice(i, 1);
-            break;
-          }
-        }
+      // socket.on('order:cancelled', function(orderData) {
+      //   console.log('Server says an order has been cancelled. Remove it!');
+      //   for (var i = 0; i < $scope.orders.length; i++) {
+      //     if ($scope.orders[i].id === orderData.id) {
+      //       $scope.orders.splice(i, 1);
+      //       break;
+      //     }
+      //   }
+      // });
+
+      socket.on('trade', function(order) {
+        console.log('Trade occurred!');
+        $scope.getBook();
       });
 
     }]);
